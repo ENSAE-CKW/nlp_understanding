73f06c8357f35bad9bdec0e3b3acff79f00323d9
khaledlarbi <khaled.larbi@live.fr>
2021-02-18 16:29:35 +0100

    add pipeline/node for data enginerring and w2v + cnn

---
+++ src/deep_nlp/pipelines/data_engineering/nodes_embed_cnn.py
@@ -0,0 +1,78 @@
+from typing import Any, Dict, List, Tuple, Set
+import numpy as np
+import spacy
+import pandas as pd
+import torch
+
+#TODO : add types to args in function
+
+def token_sentence(sentence: str, nlp: spacy.Language) -> List[str]:
+    return [X.lemma_ for X in nlp(sentence) if X.is_alpha & (not (X.is_stop))]
+
+def token_df(df: pd.DataFrame, col_name: str,nlp : spacy.Language) -> pd.DataFrame:
+    df["tokenization"] = df.apply(lambda x: token_sentence(x[col_name],nlp), axis = 1) #.lower()
+    return df
+
+#we assume that df_tokenised has a column called "tokenization"
+def vocab(df_tokenised: pd.DataFrame) -> List[str]:
+    tokenization = df_tokenised["tokenization"]
+    all_words = [element for list in tokenization for element in list]
+    return set(all_words)
+
+#We assume that the embedding is an csv file with
+#name : name of the word
+#columns named from 0 à D-1 where D stands for the dimension of the embedding
+def vectors_embed(embed: pd.DataFrame, D: int):
+    name = embed["name"]
+    vectors = embed.loc[:, [str(i) for i in range(0, D)]]
+    vectors["name"] = name
+    return name,vectors
+
+#return dic with word in embed and vocab
+#with a index
+def words_index(name_embed, vocab):
+    word_with_vectors = vocab.intersection(name_embed)
+    return dict(zip(word_with_vectors, range(len(word_with_vectors))))
+
+def index_vectors_embed(vectors, word_index_dict):
+    vectors["index"] = vectors.apply(lambda x: word_index_dict.get(x["name"]), axis=1)
+    vectors.sort_values(by = "index")
+    return vectors
+
+def token_to_index(sentence,word_index_dict):
+    return [word_index_dict.get(mots, None) for mots in sentence]
+
+def token_to_index_df(df_tokenised, word_index_dict):
+    tokenization = list(df_tokenised["tokenization"])
+    indexed = [token_to_index(sentence, word_index_dict) for sentence in tokenization]
+    df_tokenised["indexed"] = indexed
+    return df_tokenised
+
+def pad(list_word, nb):
+    if len(list_word) >= nb:
+        res = list_word[:nb]
+    else:
+        to_add = list(np.repeat(-1,nb - len(list_word)))
+        res = list_word + to_add
+    return res
+
+def pad_df(df_indexed, nb):
+    df_indexed["padded"] = df_indexed.apply(lambda x: pad(x["indexed"], nb), axis = 1)
+    return df_indexed
+
+def reshape_df(df_padded, embed_torch):
+    df = pd.DataFrame(list(df_padded["padded"]))
+    nb_row = embed_torch.shape[0] - 2
+    df = df.replace([np.nan,-1], [nb_row, nb_row + 1])
+    df["label"] = df_padded["label"]
+    return df
+
+def embed_to_torch(embed):
+    vec = embed.drop(columns=["name","index"])
+    tens = torch.from_numpy(vec.to_numpy())
+    pad_unk = torch.rand([2,tens.shape[1]])
+    tens = torch.cat([tens,pad_unk])
+    return tens
+
+def save_embed(embed):
+    return pd.DataFrame(embed.numpy())
---
+++ src/deep_nlp/pipelines/data_engineering/pipeline_embed_cnn.py
@@ -0,0 +1,126 @@
+from kedro.pipeline import Pipeline, node
+from .nodes_embed_cnn import *
+
+
+def create_embed_cnn_pipeline_de(**kwargs):
+    return Pipeline(
+        [
+            node(
+                func=token_df #verif inter
+                , inputs = ["allocine_train_short","params:colname_allocine", "nlp"]
+                , outputs = "train_tokenized"
+                , name = "tokenization"
+                , tags = ["embed_cnn","data_proc","train_set"]
+                ),
+            node(
+                func=token_df
+                , inputs=["allocine_test_short", "params:colname_allocine", "nlp"]
+                , outputs="test_tokenized"
+                , name="tokenization of test set"
+                , tags=["embed_cnn", "data_proc", "test_set"]
+            ),
+            node(
+                func=token_df
+                , inputs=["allocine_valid_short", "params:colname_allocine", "nlp"]
+                , outputs="valid_tokenized"
+                , name="tokenization of valid set"
+                , tags=["embed_cnn", "data_proc", "valid_set"]
+            ),
+            node(
+                func=vocab
+                , inputs = "train_tokenized"
+                , outputs= "vocab_set"
+                , name = "vocab initialisation"
+                , tags = ["embed_cnn", "data_proc", "train"]
+            ) ,
+            node(
+                func = vectors_embed
+                , inputs = ["word2vec","params:embed_dim"]
+                , outputs= ["name_word_in_embed", "vectors"]
+                , tags=["embed_cnn", "data_proc", "train"]
+                , name = "embedding initialisation"
+            ),
+            node(
+                func = words_index
+                , inputs = ["name_word_in_embed","vocab_set"]
+                , outputs = "word_ind_dict"
+                , tags=["embed_cnn", "data_proc", "train"]
+                , name = "index for word from voc"
+            ),
+            node(
+                func = token_to_index_df
+                , inputs = ["train_tokenized", "word_ind_dict"]
+                , outputs = "train_indexed"
+                , tags=["embed_cnn", "data_proc", "train_set"]
+                , name = "index for word from df train"
+            ),
+            node(
+                func=token_to_index_df
+                , inputs=["test_tokenized", "word_ind_dict"]
+                , outputs="test_indexed"
+                , tags=["embed_cnn", "data_proc", "test_set"]
+                , name="index for word from df test"
+            ),
+            node(
+                func=token_to_index_df
+                , inputs=["valid_tokenized", "word_ind_dict"]
+                , outputs="valid_indexed"
+                , tags=["embed_cnn", "data_proc", "test_set"]
+                , name="index for word from df valid"
+            ),
+            node(
+                func = pad_df
+                , inputs = ["train_indexed","params:sentence_size"]
+                , outputs = "train_padded"
+                , tags=["embed_cnn", "data_proc", "train_set"]
+                , name = "padding df train"),
+            node(
+                func=pad_df
+                , inputs=["test_indexed", "params:sentence_size"]
+                , outputs="test_padded"
+                , tags=["embed_cnn", "data_proc", "test_set"]
+                , name="padding df test"),
+            node(
+                func=pad_df
+                , inputs=["valid_indexed", "params:sentence_size"]
+                , outputs="valid_padded"
+                , tags=["embed_cnn", "data_proc", "valid_set"]
+                , name="padding df valid"),
+            node(
+                func = reshape_df
+                , inputs = ["train_padded","embed_for_torch"]
+                , outputs = "allocine_train_short_inter"
+                , tags = ["embed_cnn","data_proc", "train_set"]
+                , name = "reformat train"),
+            node(
+                func=reshape_df
+                , inputs=["test_padded","embed_for_torch"]
+                , outputs="allocine_test_short_inter"
+                , tags=["embed_cnn","data_proc", "test_set"]
+                , name="reformat tests"),
+            node(
+                func=reshape_df
+                , inputs=["valid_padded","embed_for_torch"]
+                , outputs="allocine_valid_short_inter"
+                , tags=["embed_cnn", "data_proc", "valid_set"]
+                , name="reformat valid"),
+            node(
+                func = index_vectors_embed
+                ,inputs = ["vectors","word_ind_dict"]
+                ,outputs = "word2vec_for_save"
+                ,tags = ["embed_cnn","data_proc","embedding"]
+                , name = "save_embed"
+            ),
+            node(
+                func = embed_to_torch
+            , inputs = "word2vec_for_save"
+            , outputs = "embed_for_torch"
+            , tags = ["embed_cnn","data_proc","embedding"]
+            , name = "embed as tensors"),
+            node(
+                func = save_embed,
+            inputs = "embed_for_torch",
+            outputs = "save_embedding",
+            tags = ["embed_cnn","data_proc","embedding"])
+        ]
+    )
