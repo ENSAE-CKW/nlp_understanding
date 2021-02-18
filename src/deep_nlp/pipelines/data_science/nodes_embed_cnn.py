731e09aa246be16c72f763e402125f9bc2384b6f
khaledlarbi <khaled.larbi@live.fr>
2021-02-18 17:02:45 +0100

    add nodes and pipeline for data science treatments

---
+++ src/deep_nlp/pipelines/data_science/nodes_embed_cnn.py
@@ -0,0 +1,142 @@
+# define metric
+import torch
+import torch.optim as optim
+import torch.nn as nn
+import logging
+from typing import Any, Dict, List, Tuple
+
+from src.deep_nlp.embed_cnn.embcnnmodel import classifier3F
+
+
+def binary_accuracy(preds, y):
+    #round predictions to the closest integer
+    rounded_preds = torch.round(preds[:,1])
+    correct = (rounded_preds == y).float()
+    acc = correct.sum() / len(correct)
+    return acc
+
+
+def train(model, iterator, optimizer, criterion):
+    # initialize epoch
+    epoch_loss = 0
+    epoch_acc = 0
+
+    # set the model in training phase
+    model.train()
+
+    for batch in iterator:
+        # resets the gradients after every batch
+        # each batch is used in order to provide an estimation of gradient C according to the paramaeters
+        optimizer.zero_grad()
+
+        # convert to 1D tensor
+        predictions = model(batch[0])
+
+        # compute the loss
+        loss = criterion(predictions[:,1], batch[1].float())
+
+        # compute the binary accuracy
+        acc = binary_accuracy(predictions, batch[1].float())
+
+        # backpropage the loss and compute the gradients
+        loss.backward()
+
+        # update the weights
+        optimizer.step()
+
+        # loss and accuracy
+        epoch_loss += loss.item()
+        epoch_acc += acc.item()
+
+    size = len(iterator)
+
+    return epoch_loss / size, epoch_acc / size
+
+
+def evaluate(model, iterator, criterion):
+    # initialize every epoch
+    epoch_loss = 0
+    epoch_acc = 0
+
+    # deactivating dropout layers
+    model.eval()
+
+    # deactivates autograd
+    with torch.no_grad():
+        for batch in iterator:
+            # retrieve text and no. of words
+            text = batch[0]
+
+            # convert to 1d tensor
+            predictions = model(text).squeeze()
+
+            # compute loss and accuracy
+            loss = criterion(predictions[:,1], batch[1].float())
+            acc = binary_accuracy(predictions, batch[1].float())
+
+            # keep track of loss and accuracy
+            epoch_loss += loss.item()
+            epoch_acc += acc.item()
+
+    size = len(iterator)
+
+    return epoch_loss / size, epoch_acc / size
+
+def init_model(embed,SENTENCE_SIZE, nb_filtre, type_filtre, nb_output, dropout):
+    type_filtre = list(type_filtre.values())
+    model = classifier3F(embed, SENTENCE_SIZE, embed.shape[1], nb_filtre, type_filtre, nb_output, dropout)
+    model = model.float()
+    return model
+
+
+def creation_batch(train_data, val_data, test_data, device, nb_batch):
+    train_tensor_x = torch.from_numpy(train_data.drop(columns=["label"]).to_numpy()).to(device).long().split(nb_batch)
+    train_tensor_y = torch.from_numpy(train_data["label"].to_numpy()).to(device).long().split(nb_batch)
+
+    val_tensor_x = torch.from_numpy(val_data.drop(columns=["label"]).to_numpy()).to(device).long().split(nb_batch)
+    val_tensor_y = torch.from_numpy(val_data["label"].to_numpy()).to(device).long().split(nb_batch)
+
+    test_tensor_x = torch.from_numpy(test_data.drop(columns=["label"]).to_numpy()).to(device).long().split(nb_batch)
+    test_tensor_y = torch.from_numpy(test_data["label"].to_numpy()).to(device).long().split(nb_batch)
+
+    train_dict = list(zip(train_tensor_x, train_tensor_y))
+    val_dict = list(zip(val_tensor_x, val_tensor_y))
+    test_dict = list(zip(test_tensor_x, test_tensor_y))
+
+    return train_dict, val_dict, test_dict
+
+
+def run_model(model, N_EPOCHS, device, train_iterator, valid_iterator):
+    best_valid_loss = float('inf')
+
+    criterion = nn.BCELoss()
+    # optimizer = optim.Adam(model.parameters())
+    optimizer = optim.Adadelta(model.parameters(), lr=1.0, rho=0.9, eps=1e-06,
+                               weight_decay=0)  # weight_decay : L2 penalisation !
+
+    model = model.to(device)
+    criterion = criterion.to(device)
+
+    best_valid_loss = float('inf')
+    best_acc = float('inf')
+    best_train_loss = float('inf')
+    best_train_acc = float('inf')
+
+    for epoch in range(N_EPOCHS):
+
+        # train the model
+        train_loss, train_acc = train(model, train_iterator, optimizer, criterion)
+
+        # evaluate the model
+        valid_loss, valid_acc = evaluate(model, valid_iterator, criterion)
+
+        # save the best model
+        if valid_loss < best_valid_loss:
+            best_valid_loss = valid_loss
+            best_acc = valid_acc
+            best_train_loss = train_loss
+            best_train_acc = train_acc
+
+        logger = logging.getLogger(__name__)
+        logger.info("Epoch %i : Accuracy : %f and Loss : %f", epoch, valid_acc,valid_loss)
+        return model
---
+++ src/deep_nlp/pipelines/data_science/pipeline_embed_cnn.py
@@ -0,0 +1,34 @@
+
+from kedro.pipeline import Pipeline, node
+
+from src.deep_nlp.pipelines.data_science.nodes_embed_cnn import *
+
+
+def create_pipeline(**kwargs):
+    return Pipeline(
+        [
+            node(
+                func = init_model,
+                inputs = ["embed_for_torch","params:sentence_size","params:embcnn_nb_filtre", "params:embcnn_type_filtre","params:embcnn_nb_output","params:embcnn_dropout"],
+                outputs = "model",
+                tags = ["embed_cnn","training", "model"],
+                name = "initiailisation model"
+            ),
+            node(
+                func = creation_batch,
+                inputs = ["allocine_train_short_inter", "allocine_valid_short_inter","allocine_test_short_inter","params:device","params:batch_size"],
+                outputs = ["train_batch", "valid_batch", "test_batch"],
+                tags=["embed_cnn", "training", "batch"],
+                name = "batch initialisation"
+            )
+            ,
+            node(
+                func = run_model,
+                inputs = ["model","params:batch_size", "params:device", "train_batch","valid_batch"],
+                outputs = "model_train",
+                tags= ["embed_cnn", "training", "model"],
+                name= "model's run"
+            )
+
+        ]
+    )
