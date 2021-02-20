c6aff25061a3a0240d91684275723ee34b6e0dbf
khaledlarbi <khaled.larbi@live.fr>
2021-02-20 21:51:19 +0100

    remove nlp.pickle from  01/raw and add creation_nlp in order to provide spacy.Language for tokenizations and automatic downlad)

--- src/deep_nlp/pipelines/data_engineering/nodes_embed_cnn.py
+++ src/deep_nlp/pipelines/data_engineering/nodes_embed_cnn.py
@@ -10,7 +10,7 @@ def creation_nlp():
     return spacy.load("fr_core_news_sm",  disable=["tagger", "parser","ner"])
 
 def token_sentence(sentence: str, nlp: spacy.language.Language) -> List[str]:
-    return [X.lemma_ for X in nlp(sentence) if X.is_alpha & (not (X.is_stop))]
+    return [X.lemmZa_ for X in nlp(sentence) if X.is_alpha & (not (X.is_stop))]
 
 def token_df(df: pd.DataFrame, col_name: str,nlp : spacy.language.Language) -> pd.DataFrame:
     df["tokenization"] = df.apply(lambda x: token_sentence(x[col_name],nlp), axis = 1) #.lower()
