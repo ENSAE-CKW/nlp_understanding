a3df5e77fc6e63b9b3f738d80b1ece1571ce1a32
Camille Francon <camille.francon@orange.fr>
2021-03-25 15:20:29 +0100

    ajout save model bilstm

--- src/deep_nlp/pipelines/data_science/pipeline_bilstm_cnn.py
+++ src/deep_nlp/pipelines/data_science/pipeline_bilstm_cnn.py
@@ -2,6 +2,7 @@ from kedro.pipeline import Pipeline, node
 
 from .nodes_bilstm_cnn import run_train
 from .nodes_bilstm_cnn import prepare_batch
+from .nodes_bilstm_cnn import save_model
 
 def create_bilstm_cnn_pipeline_ds(**kwargs):
     return Pipeline(
@@ -17,7 +18,11 @@ def create_bilstm_cnn_pipeline_ds(**kwargs):
                 inputs = ["params:cnn_cuda_allow", "train_batch", "valid_batch", "params:bilstm_num_epochs", "params:bilstm_patience", "params:bilstm_lr", "embed_matrix", "params:sentence_size", "params:bilstm_input_dim", "params:bilstm_hidden_dim", "params:bilstm_layer_dim", "params:bilstm_output_dim", "params:bilstm_feature_size", "params:bilstm_kernel_size", "params:bilstm_dropout_rate"],
                 outputs = "bilstmcnn_model",
                 tags= ["train", "bilstmcnn"],
-            )
-
+            ),
+            node(
+                func=save_model,
+                inputs="bilstmcnn_model",
+                outputs="bilstm_cnn_model_for_save"
+            ),
         ]
     )
