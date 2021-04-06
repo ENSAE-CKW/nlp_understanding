029ac878e0304d130ee22fc835d55de725757821
Orlogskapten <wenceslas.sanchez@gmail.com>
2021-04-06 11:32:36 +0200

    Update Mlflow artifact : plot GradCam global analysis for Embed CNN

---
+++ src/deep_nlp/pipelines/data_science/pipeline_embed_cnn_test.py
@@ -0,0 +1,53 @@
+# Copyright 2020 QuantumBlack Visual Analytics Limited
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+# OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, AND
+# NONINFRINGEMENT. IN NO EVENT WILL THE LICENSOR OR OTHER CONTRIBUTORS
+# BE LIABLE FOR ANY CLAIM, DAMAGES, OR OTHER LIABILITY, WHETHER IN AN
+# ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF, OR IN
+# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#
+# The QuantumBlack Visual Analytics Limited ("QuantumBlack") name and logo
+# (either separately or in combination, "QuantumBlack Trademarks") are
+# trademarks of QuantumBlack. The License does not grant you any right or
+# license to the QuantumBlack Trademarks. You may not use the QuantumBlack
+# Trademarks or any confusingly similar mark as a trademark for your product,
+# or use the QuantumBlack Trademarks in any other manner that might cause
+# confusion in the marketplace, including but not limited to in advertising,
+# on websites, or on software.
+#
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Example code for the nodes in the example pipeline. This code is meant
+just for illustrating basic Kedro features.
+
+Delete this when you start working on your own Kedro project.
+"""
+
+from kedro.pipeline import Pipeline, node
+
+from .nodes_embed_cnn import cnn_embed_test
+
+def create_embed_cnn_pipeline_test(**kwargs):
+    return Pipeline(
+        [
+            node(
+                func= cnn_embed_test
+                , inputs= ["embed_cnn_model_for_save", "test_batch"
+                    , "embed_for_torch","params:sentence_size","params:embcnn_nb_filtre"
+                    , "params:embcnn_type_filtre","params:embcnn_nb_output","params:embcnn_dropout"
+                    , "params:embcnn_padded", "word_ind_dict", "params:embcnn_class_explanation"
+                    , "params:embcnn_type_map", "params:embcnn_seuil", "params:embcnn_index_nothing"]
+                , outputs= None
+                , tags= ["embed_cnn_test", "test"]
+            )
+        ]
+    )
