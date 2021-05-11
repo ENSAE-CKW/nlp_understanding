Fixed error : Softmax model to Logsoftmax and adjust the output to proba (torch.exp)

src/deep_nlp/bilstm_cnn/bilstmcnn_gradcam.py
@@ -31,7 +31,7 @@ class BilstmCnn(GradCamBaseModel):
 
         self.fc = nn.Linear(self.linear_dim, output_dim)
 
-        self.softmax = nn.Softmax(dim=1)
+        self.softmax = nn.LogSoftmax(dim=1)
 
         # Fill up pipelines
         self.before_conv.add_module("conv", self.convLayer)

src/deep_nlp/embed_cnn/embcnnmodel_gradcam.py
@@ -42,7 +42,7 @@ class classifier3F(GradCamBaseModel):
 
         self.fc = nn.Linear(len(height_filter) * nb_filter, output_dim)
 
-        self.sm = nn.Softmax(dim=1)
+        self.sm = nn.LogSoftmax(dim=1)
 
         self.dp = nn.Dropout(p=dropout)
 

src/deep_nlp/pipelines/data_science/nodes_embed_cnn.py
@@ -39,7 +39,7 @@ def train(model, iterator, optimizer, criterion):
 
         # convert to 1D tensor
         predictions = model(variables_x)
-
+        predictions = torch.exp(predictions)
         # compute the loss
         loss = criterion(predictions[:,1], variables_y.float())
 
@@ -77,6 +77,7 @@ def evaluate(model, iterator, criterion):
 
             # convert to 1d tensor
             predictions = model(text).squeeze()
+            predictions = torch.exp(predictions)
 
             # compute loss and accuracy
             loss = criterion(predictions[:,1], variables_y.float())
@@ -203,6 +204,7 @@ def cnn_embed_test(model_dict, iterator
     for review, label in iterator:
 
         output = model(review)
+        output= torch.exp(output)
 
         predict = torch.max(output, 1)[1].view(label.size()).data
         corrects += (predict == label.data).sum()